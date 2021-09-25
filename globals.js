fe8bd80fa46fb4a438b4b0a598a7f5be5587ab6a
Levente Novotny <Novotny.Levente@gmail.com>
2021-09-25 11:37:19 +0200

    Show stats

--- globals.js
+++ globals.js
@@ -3,7 +3,8 @@ exports.spriteId = 1; //Which sprite are we working on //!Do not modify
 
 //? These variables are settings, you are safe to modify them
 exports.exportProjectFile = true; //Save sb3 file
-exports.delayYAxis = 0.0; //Delay on y axis
-exports.delayXAxis = 0.0; //Delay pixel drawing on x axis
+exports.delayYAxis = 0.001; //Delay on y axis
+exports.delayXAxis = 0.0001; //Delay pixel drawing on x axis
 exports.delayXEveryXFrame = 50; //Only delay on x axis if currPixel is divisable by this number
 exports.splitSpritesByYLines = 1; //Create new sprite every Y lines
+exports.showStats = true; //Show stats after code gen
--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const Jimp = require('jimp');
 const fs = require('fs');
 const zipper = require("zip-local");
+const nanocolors = require('nanocolors');
+const fastFolderSize = require('fast-folder-size');
+const { promisify } = require('util');
+const fastFolderSizeAsync = promisify(fastFolderSize);
 const scratchFunctions = {
     penDown: require('./blocks/penDown').penDown,
     penUp: require('./blocks/penUp').penUp,
@@ -38,7 +42,7 @@ async function main() {
     const imageStartY = 0 - ((imageX / 2) * 2); // Center image, *2 because each pixel in image is drawn by 2 pixels in scratch
     for (let imgY = 0; imgY <= imageY; imgY++) {
         //Move to next line
-        console.log("Line Y", imgY);
+        console.log(`${nanocolors.gray("Line Y")}`, nanocolors.gray(imgY));
         if (imgY != 0 && imgY % globals.splitSpritesByYLines === 0) {
             scratchCodeJson = scratchFunctions.hide(scratchCodeJson);
             scratchCodeJson = scratchFunctions.addNewSprite(scratchCodeJson);
@@ -60,21 +64,38 @@ async function main() {
     scratchCodeJson = scratchFunctions.hide(scratchCodeJson);
 
     // Write output to file
-    console.log("Writing output...");
+    console.log(nanocolors.cyan("Writing output..."));
     fs.writeFileSync('./output/uncompressed/project.json', JSON.stringify(scratchCodeJson), 'utf8');
     fs.copyFileSync('./resources/54b235d3219beb83bc6212d32adc07e9.svg', './output/uncompressed/54b235d3219beb83bc6212d32adc07e9.svg');
     fs.copyFileSync('./resources/cd21514d0531fdffb22204e0ec5ed84a.svg', './output/uncompressed/cd21514d0531fdffb22204e0ec5ed84a.svg');
-    console.log("Writing done");
+    console.log(nanocolors.green("Writing done"));
+    console.log(`${nanocolors.green("Uncompressed files created")} ${nanocolors.yellow("(./output/uncompressed/)")}`);
 
     if (globals.exportProjectFile) {
-        console.log("Compressing project file...");
+        console.log(nanocolors.cyan("Compressing project file..."));
         let zipFiles = zipper.sync.zip("./output/uncompressed/");
         zipFiles.compress();
         zipFiles.save_async = false;
         zipFiles.save("./output/output.sb3");
-        console.log("SB3 file created (./output/output.sb3), have a nice day!");
-    } else {
-        console.log("Uncompressed files created (./output/uncompressed/), have a nice day!");
+        console.log(`${nanocolors.green("SB3 file created")} ${nanocolors.yellow("(./output/output.sb3)")}`);
+    }
+
+    if (globals.showStats) {
+        console.log(nanocolors.gray("\n------------------------------\n"));
+        console.log(nanocolors.green("Project stats:\n"));
+        console.log(nanocolors.green("Resized img resolution: "), nanocolors.yellow(`${imageX}*${imageY} =`), nanocolors.red(`${imageX * imageY}px`));
+        console.log(nanocolors.green("Sprites: "), nanocolors.yellow(scratchCodeJson.targets.length - 1));
+        console.log(nanocolors.green("Messages: "), nanocolors.yellow(Object.keys(scratchCodeJson.targets[0].broadcasts).length));
+        const blockCount = scratchCodeJson.targets.map((curr) =>
+            Object.keys(curr.blocks).length
+        ).reduce((a, b) => a + b);
+        console.log(nanocolors.green("Blocks: "), nanocolors.yellow(blockCount));
+        const unCompressedBytes = await fastFolderSizeAsync('./output/uncompressed');
+        console.log(nanocolors.green("Uncompressed code: "), nanocolors.yellow(`${unCompressedBytes / 1000} kb`));
+        if (globals.exportProjectFile) {
+            const compressedBytes = fs.statSync("./output/output.sb3").size;
+            console.log(nanocolors.green("Compressed SB3 code: "), nanocolors.yellow(`${compressedBytes / 1000} kb`), nanocolors.magenta(`(${(100 - ((compressedBytes / unCompressedBytes) * 100)).toFixed(3)}% saving)`));
+        }
     }
 }
 
