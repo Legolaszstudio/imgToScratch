9582f5f57cf237ae6885180100513e432d72eb4a
Levente Novotny <Novotny.Levente@gmail.com>
2021-09-25 10:21:42 +0200

    Different delay on X and Y axis

--- globals.js
+++ globals.js
@@ -2,6 +2,6 @@ exports.lastBlockLength = 3; //Last block id in template file //!Do not modify
 exports.spriteId = 1; //Which sprite are we working on //!Do not modify
 //? These variables are settings, you are safe to modify them
 exports.exportProjectFile = true; //Save sb3 file
-exports.delay = 0.001; //Delay on y axis (and x if next flag is set)
-exports.delayXAxis = true; //Delay pixel drawing on x axis
-exports.delayXEveryXFrame = 25; //Only delay on x axis if currPixel is divisable by this number
+exports.delayYAxis = 0; //Delay on y axis
+exports.delayXAxis = 0.001; //Delay pixel drawing on x axis
+exports.delayXEveryXFrame = 50; //Only delay on x axis if currPixel is divisable by this number
